af0bd71ec1f7cb7c76aff80300a75eead946d3fa
Vikashmehta0601 <vikashmehta0601@gmail.com>
2025-01-16 18:30:26 +0530

    Update Projects.js

--- git/src/content/Projects.js
+++ git/src/content/Projects.js
@@ -4,14 +4,14 @@ const ProjectsData = [
   {
     id: 1,
     name: "Hop Project",
-    summary: " A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.",
+    summary: "Hop Electric is India's leading two wheeler electric vehicle & charging battery scooter company. Dynamic Booking System: Created an intuitive interface for users to book test rides for EV bikes online. Interactive User Experience: Used React.js to provide a seamless and engaging booking process.",
     link: "https://hopelectric.in/",
     projectImage:IMAGES.hopImage,
   },
   {
     id: 2,
     name: "TRP World",
-    summary: " A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.",
+    summary: "Simplified the hotel booking process for users. Increased engagement and conversion rates with a responsive and visually appealing design. Provided a scalable and maintainable solution for hotel operators to manage bookings efficiently.",
     link: "https://trpworld.com/login",
     projectImage:IMAGES.trpImage,
   },
