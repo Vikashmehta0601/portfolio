ca87f053fe543734c541d2f802f9fa22bac92a14
Vikashmehta0601 <vikashmehta0601@gmail.com>
2025-01-20 13:47:20 +0530

    Update WhatsappINT.jsx

--- git/src/components/whatsappconnect/WhatsappINT.jsx
+++ git/src/components/whatsappconnect/WhatsappINT.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
-import "./style.css"
+import "./style.css";
+import { IoCall } from "react-icons/io5";
 
 const WhatsappINT = () => {
-
-  
   return (
-    <div className="whatsapp_section" onClick={() =>
-      window.open("https://wa.me/6376415198", "_blank")
-    }>
-      {/* WA */}
-      <FaWhatsapp />
+    <div className="floating_icons">
+      <div
+        className="whatsapp_section"
+        onClick={() => window.open("https://wa.me/6376415198", "_blank")}
+      >
+        <FaWhatsapp />
+      </div>
+      <div
+        className="whatsapp_section calls_section"
+        onClick={() => window.open("https://wa.me/6376415198", "_blank")}
+      >
+        <IoCall />
+      </div>
     </div>
   );
 };
