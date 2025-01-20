283f4ede2f00992b6472a785d1cb5f78334c4238
Vikashmehta0601 <vikashmehta0601@gmail.com>
2025-01-20 13:10:49 +0530

    Update App.jsx

--- git/src/App.jsx
+++ git/src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/Home'
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-
-  return (
-    <>
-      <Home/>
-    </>
-  )
+  return <Home />;
 }
 
-export default App
+export default App;
