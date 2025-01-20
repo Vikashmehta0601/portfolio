c241315bfde091c188224db8a77ce2d86560d0ba
Vikashmehta0601 <vikashmehta0601@gmail.com>
2025-01-20 18:47:26 +0530

    Update WhatsappINT.jsx

--- git/src/components/whatsappconnect/WhatsappINT.jsx
+++ git/src/components/whatsappconnect/WhatsappINT.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import "./style.css";
 import { IoCall } from "react-icons/io5";
+import CustomBootstrapModal from "../modal/CustomBootstrapModal";
 
 const WhatsappINT = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div className="floating_icons">
-      <div
-        className="whatsapp_section"
-        onClick={() => window.open("https://wa.me/6376415198", "_blank")}
-      >
-        <FaWhatsapp />
+    <>
+      <div className="floating_icons">
+        <div
+          className="whatsapp_section"
+          onClick={() => window.open("https://wa.me/6376415198", "_blank")}
+        >
+          <FaWhatsapp />
+        </div>
+        <div
+          className="whatsapp_section calls_section"
+          onClick={() => window.open("tel:+6376415198", "_self")}
+        >
+          <IoCall />
+        </div>
       </div>
-      <div
-        className="whatsapp_section calls_section"
-        onClick={() => window.open("https://wa.me/6376415198", "_blank")}
-      >
-        <IoCall />
-      </div>
-    </div>
+
+      {/* <CustomBootstrapModal Header={"Contact"} size={"lg"} show={show} setShow={setShow}/> */}
+    </>
   );
 };
 
