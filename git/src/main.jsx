eb4cb5d0c846e0bf67c060e47d21808d322d2c23
Vikashmehta0601 <vikashmehta0601@gmail.com>
2025-01-20 13:11:09 +0530

    Update main.jsx

--- git/src/main.jsx
+++ git/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import Home from "./pages/Home.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<div>404 - Page Not Found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  </>
+);
