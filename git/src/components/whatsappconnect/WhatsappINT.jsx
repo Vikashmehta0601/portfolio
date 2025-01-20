0e9482e655ce89b4baf97ef772fa36e91c32e45c
Vikashmehta0601 <vikashmehta0601@gmail.com>
2025-01-20 13:24:54 +0530

    Add files via upload

---
+++ git/src/components/whatsappconnect/WhatsappINT.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import { FaWhatsapp } from "react-icons/fa";
+import "./style.css"
+
+const WhatsappINT = () => {
+
+  // onClick={() =>
+  //   window.open("https://wa.me/6376415198", "_blank")
+  // }
+  return (
+    <div className="whatsapp_section">
+      WA
+      {/* <FaWhatsapp /> */}
+    </div>
+  );
+};
+
+export default WhatsappINT;
